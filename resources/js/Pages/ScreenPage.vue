6191487969981de2ddcec56068179d96ce1c0935
Mathijs Markvoort <75422017+mmathijs@users.noreply.github.com>
2024-03-21 09:53:19 +0100

    Mooi

--- resources/js/Pages/ScreenPage.vue
+++ resources/js/Pages/ScreenPage.vue
@@ -46,7 +46,9 @@ onMounted(() => {
         <div class="w-screen h-screen flex items-center">
             <div class="text-center my-auto mx-auto gap-1 flex flex-col" v-if="currentAct.display_type=== 'ActSingle'">
                 <h2 class="text-4xl text-gray-300 font-bold">{{ currentAct.type }}</h2>
-                <h1 class="text-7xl font-semibold leading-none" v-html="currentAct.name"></h1>
+                <h1 class=" font-semibold leading-none" v-html="currentAct.name"
+                    :class="currentAct.people == currentAct.name ? 'text-8xl mb-4' : 'text-7xl'"
+                ></h1>
 <!--                {{currentAct.people + ' ' + currentAct.name}}{{currentAct.name.toString().trimStart().trimEnd() === currentAct.people.toString().trimStart().trimEnd() }}-->
                 <p class="text-4xl mt-4 font-semibold" v-if="currentAct.people !== currentAct.name">{{ currentAct.people }}</p>
             </div>
